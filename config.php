2f5c2b8c090eddf9fc3cee15529c60d143b80f94
dimka-miracle <mr.miracle@inbox.ru>
2012-12-12 23:26:19 +0400

    upd

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-define("DB_HOST",'');   // ip базы данных
-define("DB_ADMIN",'');  // логин админа БД
-define("DB_PASS",'');   // пароь админа БД  
-define("DB_USER",'');   // имя пользователя БД
-?>
